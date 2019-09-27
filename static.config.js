f4ae9afd8e3840b138f2699e62d6e898d8880e02
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2019-09-27 15:32:08 +0200

    Add sitemap

--- static.config.js
+++ static.config.js
@@ -79,6 +79,7 @@ export default {
     ];
   },
   plugins: [
-    require.resolve('react-static-plugin-reach-router')
+    require.resolve('react-static-plugin-reach-router'),
+    require.resolve('react-static-plugin-sitemap'),
   ]
 };
